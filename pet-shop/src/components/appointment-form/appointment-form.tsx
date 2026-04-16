43921c27ad51d16cb49bd17bae10af655926096f
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2026-04-16 11:46:13 +0200

    settting new components
    form, input, text area for modal
    z and schema validation

--- pet-shop/src/components/appointment-form/appointment-form.tsx
+++ pet-shop/src/components/appointment-form/appointment-form.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import z from 'zod';
 import { Button } from '../ui/button';
 import {
   Dialog,
@@ -9,8 +10,44 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '../ui/dialog';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form';
+import { Dog, User } from 'lucide-react';
+import { Input } from '../ui/input';
+import { Textarea } from '../ui/textarea';
+
+const appoimentFormSchema = z.object({
+  tutorName: z.string().min(3, 'Tutor name is required'),
+  petName: z.string().min(3, 'Pet name is required'),
+  phone: z.string().min(11, 'Phone number is required'),
+  description: z.string().min(3, 'Description is required'),
+});
+
+type AppointmentFormValues = z.infer<typeof appoimentFormSchema>;
 
 export const AppointmentForm = () => {
+  const form = useForm<AppointmentFormValues>({
+    resolver: zodResolver(appoimentFormSchema),
+    defaultValues: {
+      tutorName: '',
+      petName: '',
+      phone: '',
+      description: '',
+    },
+  });
+
+  const onSubmit = (data: AppointmentFormValues) => {
+    console.log(data);
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -28,6 +65,84 @@ export const AppointmentForm = () => {
             Fill all the client's data to make the appointment
           </DialogDescription>
         </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="tutorName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-label-medium-size text-content-primary">
+                    Tutor Name
+                  </FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <User
+                        className="absolute left-3 top-1/2 -translate-y-1/2 text-content-brand transform"
+                        size={20}
+                      />
+                      <Input
+                        placeholder="Tutors name"
+                        className="pl-10"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="petName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-label-medium-size text-content-primary">
+                    Pet Name
+                  </FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <Dog
+                        className="absolute left-3 top-1/2 -translate-y-1/2 text-content-brand transform"
+                        size={20}
+                      />
+                      <Input
+                        placeholder="Tutors name"
+                        className="pl-10"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-label-medium-size text-content-primary">
+                    Description
+                  </FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <Textarea
+                        placeholder="service description"
+                        className="resize-none"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
